e71c01c60da92aff8357282f96fde92052abd540
Nicolas Martyanoff <khaelin@gmail.com>
2021-10-27 13:55:43 +0200

    add missing file

---
+++ js/evweb/evweb.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burgers = document.querySelectorAll(".navbar-burger");
+
+  burgers.forEach(burger => {
+    burger.addEventListener("click", () => {
+      const targetId = burger.dataset.target;
+      const target = document.getElementById(targetId);
+
+      burger.classList.toggle("is-active");
+      target.classList.toggle("is-active");
+    });
+  });
+});
